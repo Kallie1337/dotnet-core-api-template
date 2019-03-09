18b9742be5dec0fa8b785e80ddd72c3fdef09073
Kallie1337 <kallie@nextborder.xyz>
2019-03-09 11:13:11 +0100

    Mapping profiles for AutoMapper DTOs

---
+++ Utils/MappingProfiles.cs
@@ -0,0 +1,13 @@
+using System;
+using AutoMapper;
+
+namespace crushtown_heroes_API.Utils
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            // CreateMap<Object, ObjectDTO>().ReverseMap();
+        }
+    }
+}
